2d368df4cb07018c6c5bc2f0ea7698a8116721a1
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2019-07-14 09:05:14 +0900

    Update SampleControl

--- Sample/SampleControl.cs
+++ Sample/SampleControl.cs
@@ -21,15 +21,18 @@ namespace Sample {
             resCache.Add( "BlueBrush" , t => new SolidColorBrush( t, new RawColor4( 0.0f, 0.0f, 1.0f, 1.0f ) ) );
         }
 
-        public override void Render( RenderTarget target ) {
-            target.Clear( new RawColor4( 1.0f, 1.0f, 1.0f, 1.0f ) );
+        public override void Render(DeviceContext target)
+        {
+            var renderTarget = resCache.RenderTarget;
+
+            renderTarget.Clear( new RawColor4( 1.0f, 1.0f, 1.0f, 1.0f ) );
             Brush brush = null;
             switch( rnd.Next( 3 ) ) {
                 case 0: brush = resCache["RedBrush"  ] as Brush; break;
                 case 1: brush = resCache["GreenBrush"] as Brush; break;
                 case 2: brush = resCache["BlueBrush" ] as Brush; break;
             }
-            target.DrawRectangle( new RawRectangleF( x, y, x + w, y + h ), brush );
+            renderTarget.DrawRectangle( new RawRectangleF( x, y, x + w, y + h ), brush );
 
             x = x + dx;
             y = y + dy;
